81713ca0cd93fb3775cba1c2d514e735244189fc
FAVOUR <truelyfavoured@gmail.com>
2019-11-27 06:50:52 +0100

    Set up views with dataBinding

--- app/src/main/java/com/example/todo/tasks/TaskActivity.kt
+++ app/src/main/java/com/example/todo/tasks/TaskActivity.kt
@@ -1,7 +1,8 @@
-package com.example.todo
+package com.example.todo.tasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todo.R
 
 class TaskActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/example/todo/tasks/TaskAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.todo.tasks
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by Olije Favour on 11/26/2019.
+ *Copyright (c) 2019    All rights reserved.
+ */
+
+// *Copyright (c) 2019  Itex Integrated Services  All rights reserved.
+
+
+class TaskAdapter: RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+    }
+
+    inner class TaskViewHolder(view:View):RecyclerView.ViewHolder(view)
+}
