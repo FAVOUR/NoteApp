d6dd7db0c4c523a24207c03e35064d234d95a14c
FAVOUR <truelyfavoued@gmail.com>
2019-12-05 06:55:57 +0100

    Partial implimentation for the Navigation controller Class

--- app/src/main/java/com/example/todo/tasks/TaskActivity.kt
+++ app/src/main/java/com/example/todo/tasks/TaskActivity.kt
@@ -2,8 +2,15 @@ package com.example.todo.tasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.todo.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Main activity for the todoapp. Holds the Navigation Host Fragment and the Drawer, Toolbar, etc.
@@ -17,6 +24,19 @@ class TaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupDrawerLayout()
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
+
+        val navController:NavController =findNavController(R.id.NavigationHost)
+        val appBarConfiguration:AppBarConfiguration=AppBarConfiguration.Builder(R.id.task_details_fragment,R.id.staticsticsFragment,R.id.add_Edit_Task_Fragment)
+                                                        .setDrawerLayout(navDrawer)
+                                                         .build()
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+
+        navigationView.setupWithNavController(navController)
+
+
     }
 
     private fun setupDrawerLayout() {
