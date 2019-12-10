9db95868588e531364b59256679dd20974733f1c
FAVOUR <truelyfavoured@gmail.com>
2019-12-10 05:51:32 +0100

    Added the task_fragment menu and the viewmodel for fragment

--- app/src/main/java/com/example/todo/tasks/TaskFragment.kt
+++ app/src/main/java/com/example/todo/tasks/TaskFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.todo.R
 import com.example.todo.databinding.FragmentTaskBinding
 
@@ -16,12 +17,19 @@ class TaskFragment : Fragment() {
 
     lateinit var viewFragmentBinding:FragmentTaskBinding
 
+    private val  viewModel by viewModels<TaskViewModel> ()
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_task, container, false)
-        viewFragmentBinding= FragmentTaskBinding.inflate(inflater, container, false)
+        viewFragmentBinding= FragmentTaskBinding.inflate(inflater, container, false).apply {
+
+          viewmodel=viewModel
+        }
 
         return viewFragmentBinding.root
     }
