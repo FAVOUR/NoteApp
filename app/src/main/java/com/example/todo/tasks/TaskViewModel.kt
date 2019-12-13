fb0526e83182b0d78c8c846dc802a356bd4ef4eb
FavourItex <truelyFavoured@gmail.com>
2019-12-13 07:03:08 +0100

    Partially implimented the TaskViewModel

--- app/src/main/java/com/example/todo/tasks/TaskViewModel.kt
+++ app/src/main/java/com/example/todo/tasks/TaskViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.todo.tasks
 
 import android.app.Application
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.lifecycle.*
 import com.example.todo.data.Result
 import com.example.todo.data.Task
@@ -16,6 +18,13 @@ import com.example.todo.data.Result.Success
 
 
 class TaskViewModel(application: Application):AndroidViewModel(application) {
+
+
+    init {
+
+        loadTasks(true)
+
+    }
 
 
    private val taskRepository=DefaultTaskRepository.getRepository(application)
@@ -44,12 +53,22 @@ class TaskViewModel(application: Application):AndroidViewModel(application) {
     }
 
 
+    /**
+     * @param forceUpdate   Pass in true to refresh the data in the [TasksDataSource]
+     */
+    fun loadTasks(forceUpdate: Boolean) {
+        _forceUpdate.value = forceUpdate
+    }
+
+
     var _isDataLoading = MutableLiveData<Boolean>()
     val isDataLoading :LiveData<Boolean> = _isDataLoading
 
     private var _isDataLoadingError  = MutableLiveData<Boolean>()
 
     val isDataLoadingError = _isDataLoadingError
+
+    var itemListOfTasks:LiveData<List<Task>> = _items
 
 
 
@@ -71,6 +90,34 @@ private fun filterTask(taskResult:Result<List<Task>>):LiveData<List<Task>>{
 
 
   return  result
+    }
+
+
+    private fun c (taskType:TaskFilterType){
+
+        currentfiltering=taskType
+
+        when(taskType){
+
+            TaskFilterType.COMPLETED_TASK->{
+                setFilter(R., )
+            }
+
+            TaskFilterType.ALLTASK->{
+
+            }
+
+            TaskFilterType.ACTIVE_TASK->{
+
+                }
+
+        }
+
+    }
+
+
+    private fun setFilter(@StringRes filtringLabelString:Int, @StringRes noTaskLabelString:Int,@DrawableRes noTaskIconVariable:Int,taskAddVisible:Boolean){
+
     }
 
     private fun  filterItems(itemList:List<Task>,filteringType:TaskFilterType):List<Task>{
