830093318d0e93eebb3ea3e4336984f4543d70f8
FavourItex <truelyFavoured@gmail.com>
2019-12-15 03:23:10 +0100

    Added all drawable file for the project partially implimenting viewmodel for the project

--- app/src/main/java/com/example/todo/tasks/TaskViewModel.kt
+++ app/src/main/java/com/example/todo/tasks/TaskViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.todo.tasks
 
 import android.app.Application
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.lifecycle.*
 import com.example.todo.data.Result
@@ -9,6 +8,8 @@ import com.example.todo.data.Task
 import com.example.todo.data.source.DefaultTaskRepository
 import kotlinx.coroutines.launch
 import com.example.todo.data.Result.Success
+import com.example.todo.R
+
 
 /**
  * Created by Olije Favour on 12/1/2019.
@@ -23,7 +24,6 @@ class TaskViewModel(application: Application):AndroidViewModel(application) {
     init {
 
         loadTasks(true)
-
     }
 
 
@@ -32,6 +32,19 @@ class TaskViewModel(application: Application):AndroidViewModel(application) {
     private var currentfiltering=TaskFilterType.COMPLETED_TASK
 
     private val _forceUpdate = MutableLiveData<Boolean>(false)
+
+    private val _currentFilteringLabel = MutableLiveData<Int>()
+    val currentFilteringLabel: LiveData<Int> = _currentFilteringLabel
+
+    private val _noTasksLabel = MutableLiveData<Int>()
+    val noTasksLabel: LiveData<Int> = _noTasksLabel
+
+    private val _noTaskIconRes = MutableLiveData<Int>()
+    val noTaskIconRes: LiveData<Int> = _noTaskIconRes
+
+    private val _tasksAddViewVisible = MutableLiveData<Boolean>()
+    val tasksAddViewVisible: LiveData<Boolean> = _tasksAddViewVisible
+
 
 
     private val _items:LiveData<List<Task>> = _forceUpdate.switchMap { forceUpdate ->
@@ -93,14 +106,14 @@ private fun filterTask(taskResult:Result<List<Task>>):LiveData<List<Task>>{
     }
 
 
-    private fun c (taskType:TaskFilterType){
+    private fun startFiltering (taskType:TaskFilterType){
 
         currentfiltering=taskType
 
         when(taskType){
 
             TaskFilterType.COMPLETED_TASK->{
-                setFilter(R., )
+                setFilter(R.string.label_all,R.string.)
             }
 
             TaskFilterType.ALLTASK->{
@@ -116,9 +129,60 @@ private fun filterTask(taskResult:Result<List<Task>>):LiveData<List<Task>>{
     }
 
 
-    private fun setFilter(@StringRes filtringLabelString:Int, @StringRes noTaskLabelString:Int,@DrawableRes noTaskIconVariable:Int,taskAddVisible:Boolean){
+    private fun setFilter(@StringRes filtringLabelString:Int, @StringRes noTaskLabelString:Int,noTaskIconVariable:Int,taskAddVisible:Boolean){
 
     }
+
+
+
+
+
+//    /**
+//     * Sets the current task filtering type.
+//     *
+//     * @param requestType Can be [TasksFilterType.ALL_TASKS],
+//     * [TasksFilterType.COMPLETED_TASKS], or
+//     * [TasksFilterType.ACTIVE_TASKS]
+//     */
+//    fun setFiltering(requestType: TasksFilterType) {
+//        currentFiltering = requestType
+//
+//        // Depending on the filter type, set the filtering label, icon drawables, etc.
+//        when (requestType) {
+//            TaskFilterType.ALLTASK -> {
+//                setFilter(
+//                    R.string.label_all, R.string.no_tasks_all,
+//                    R.drawable.logo_no_fill, true
+//                )
+//            }
+//            TaskFilterType.ACTIVE_TASK -> {
+//                setFilter(
+//                    R.string.label_active, R.string.no_tasks_active,
+//                    R.drawable.ic_check_circle_96dp, false
+//                )
+//            }
+//            TaskFilterType.COMPLETED_TASK -> {
+//                setFilter(
+//                    R.string.label_completed, R.string.no_tasks_completed,
+//                    R.drawable.ic_verified_user_96dp, false
+//                )
+//            }
+//        }
+//        // Refresh list
+//        loadTasks(false)
+//    }
+//
+//    private fun setFilter(
+//        @StringRes filteringLabelString: Int, @StringRes noTasksLabelString: Int,
+//        @DrawableRes noTaskIconDrawable: Int, tasksAddVisible: Boolean
+//    ) {
+//        _currentFilteringLabel.value = filteringLabelString
+//        _noTasksLabel.value = noTasksLabelString
+//        _noTaskIconRes.value = noTaskIconDrawable
+//        _tasksAddViewVisible.value = tasksAddVisible
+//    }
+
+
 
     private fun  filterItems(itemList:List<Task>,filteringType:TaskFilterType):List<Task>{
 
